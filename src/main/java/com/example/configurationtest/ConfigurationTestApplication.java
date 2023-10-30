e5cd7d0a15c9f7a80b804466341277dec4b4cc9d
Yong Yang <yangy153@chinatelecom.cn>
2023-10-30 11:08:20 +0800

    修改配置类，配置计量单位

--- src/main/java/com/example/configurationtest/ConfigurationTestApplication.java
+++ src/main/java/com/example/configurationtest/ConfigurationTestApplication.java
@@ -3,13 +3,17 @@ package com.example.configurationtest;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ServerConfig.class)
 public class ConfigurationTestApplication {
 
     @Bean
+    @ConfigurationProperties(prefix = "datasource")
     public DruidDataSource druidDataSource(){
         DruidDataSource ds = new DruidDataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
--- src/main/java/com/example/configurationtest/ServerConfig.java
+++ src/main/java/com/example/configurationtest/ServerConfig.java
@@ -2,17 +2,28 @@ package com.example.configurationtest;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.boot.convert.DurationUnit;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 /**
  * @author YangY
  */
-@Component
+//@Component
 @Data
 @ConfigurationProperties(prefix = "servers")
 public class ServerConfig {
     private String ipAddress;
     private int port;
     private long timeout;
+    @DurationUnit(ChronoUnit.HOURS)
+    private Duration serverTimeout;
+    @DataSizeUnit(DataUnit.KILOBYTES)
+    private DataSize dataSize;
 }
