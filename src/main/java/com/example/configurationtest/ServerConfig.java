f0c7d2b9364e609768a9f62cb08725b2d078e583
Yong Yang <yangy153@chinatelecom.cn>
2023-10-30 14:41:40 +0800

    修改计量单位

--- src/main/java/com/example/configurationtest/ServerConfig.java
+++ src/main/java/com/example/configurationtest/ServerConfig.java
@@ -8,18 +8,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.util.unit.DataSize;
 import org.springframework.util.unit.DataUnit;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 /**
  * @author YangY
+ * @author YangY
  */
 //@Component
 @Data
 @ConfigurationProperties(prefix = "servers")
+/**
+ * 开启对当前Bean的属性注入校验
+ */
+@Validated
 public class ServerConfig {
     private String ipAddress;
+    @Max(value = 8888, message = "最大值不能超过8888")
     private int port;
     private long timeout;
     @DurationUnit(ChronoUnit.HOURS)
--- src/test/java/com/example/configurationtest/ConfigurationTestApplicationTests.java
+++ src/test/java/com/example/configurationtest/ConfigurationTestApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.configurationtest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.validation.annotation.Validated;
 
 @SpringBootTest
 class ConfigurationTestApplicationTests {
 
+    @Value("${datasource.password}")
+    private String password;
+
+
     @Test
     void contextLoads() {
+        System.out.println(password);
     }
 
 }
